f5875760ddd3a41711d7f958add4034a71c52a56
Ian David Wild <ian.wild@heavy-horse.co.uk>
2012-11-22 15:24:28 +0000

    Update version.php
    Updated version number (XX incremented).

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012072700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012072701;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'gradereport_marking'; // Full name of the plugin (used for diagnostics)
